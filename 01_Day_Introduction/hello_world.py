c9b5335450de01b5bfbce5a67e92199a13e8f522
Dylan <drossi517@gmail.com>
2021-01-24 14:25:19 -0800

    Day 1 exercises completed

---
+++ 01_Day_Introduction/hello_world.py
@@ -0,0 +1,33 @@
+#   Dylan Rossi
+#   Day 1 Exercises: 30 Days of Python
+#   1.24.21
+
+#   Part 1: Mathematical Operations Practice:
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(4 % 3)
+print(3 / 4)
+print(4 / 3)
+print(3 ** 4)
+print(3 // 4)
+print(4 // 3)
+
+#   Part 2: Writing strings:
+print("Dylan")
+print("Rossi")
+print('United States of America')
+print('I am enjoying 30 days of Python')
+
+#   Part 3: Checking Data Types:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Dylan', 'Python', 'Finland']))
+print(type('Dylan'))
+print(type("Rossi"))
+print(type('United States of America'))
+
+
